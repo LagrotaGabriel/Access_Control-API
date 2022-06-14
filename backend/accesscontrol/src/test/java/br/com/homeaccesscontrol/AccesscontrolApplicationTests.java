b3ad7a2f1104a5fc31d15b3fef572ff41ffa66dd
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-06-14 08:31:15 -0300

    Spring initializer created

---
+++ backend/accesscontrol/src/test/java/br/com/homeaccesscontrol/AccesscontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.homeaccesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesscontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
