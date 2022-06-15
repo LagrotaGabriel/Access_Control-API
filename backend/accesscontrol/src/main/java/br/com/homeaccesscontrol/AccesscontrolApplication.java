1200edef3f10e2951bcb49f4e5540635f5fd2720
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-06-15 17:58:01 -0300

    UML Atualization

--- backend/accesscontrol/src/main/java/br/com/homeaccesscontrol/AccesscontrolApplication.java
+++ backend/accesscontrol/src/main/java/br/com/homeaccesscontrol/AccesscontrolApplication.java
@@ -2,8 +2,15 @@ package br.com.homeaccesscontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("br.com.homeaccesscontrol.repositories")
+@EntityScan(basePackages = {"br.com.homeaccesscontrol.models"})
+@ComponentScan("br.com.homeaccesscontrol.services")
+@ComponentScan("br.com.homeaccesscontrol.controllers")
 public class AccesscontrolApplication {
 
 	public static void main(String[] args) {
